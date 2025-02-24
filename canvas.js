3d2f13ae1c77ca759da224214c993324192ccca0
Robert Bonglamphone <8757841+bonglrob@users.noreply.github.com>
2025-02-24 14:45:29 -0800

    feat(canvas): add button to ask user to resize the canvas grid size

--- canvas.js
+++ canvas.js
@@ -2,10 +2,14 @@
     window.addEventListener("load", init);
 
     function init() {
-        const wrapperContainer = qs("main > .wrapper");
-        const panels = genPanels(256);
-    
-        wrapperContainer.appendChild(panels);
+        genCanvas();
+
+        const gridSizeButton = id("grid-size");
+        gridSizeButton.addEventListener("click", genCanvas);
+    }
+
+    function id(element) {
+        return document.getElementById(element);
     }
 
     function qs(selector) {
@@ -14,6 +18,15 @@
 
     function gen(tagName) {
         return document.createElement(tagName);
+    }
+
+    function removeCanvas() {
+        const wrapperContainer = qs("main > .wrapper");
+        wrapperContainer.innerHTML = "";
+    }
+
+    function getGridSize() {
+        return numPerRow = prompt("How many squares do you want per row? (e.g. 2 for a 2 x 2 Grid)");
     }
 
     function genContainer() {
@@ -27,15 +40,31 @@
         panel.classList.toggle("panel--bg-color-green");
     }
 
-    function genPanels(times) {
+    function genPanels(numPerRow) {
         const container = genContainer();
+        const totalPanels = numPerRow * numPerRow;
 
-        for (let i = 0; i < times; i++) {
+        for (let i = 0; i < totalPanels; i++) {
             let panel = gen("div");
             panel.classList.add("panel");
+            // panel.style.flex = `1 0 ${(1 / numPerRow - 0.001) * 100}`;
+
             panel.addEventListener("mouseover", changePanelColor);
             container.appendChild(panel);
         }
         return container;
     }
+
+    function genCanvas() {
+        const wrapperContainer = qs("main > .wrapper");
+        let numPerRow = 16; // 16 x 16 grid = 256 total panels
+        if (wrapperContainer.hasChildNodes()) {
+            numPerRow = getGridSize();
+            removeCanvas();
+        }
+
+        const panels = genPanels(numPerRow);
+        wrapperContainer.appendChild(panels);
+    }
+
 })();
